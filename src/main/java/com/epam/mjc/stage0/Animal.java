4eac8a9bd67de139aa50f0300fffb280a059c7ff
LChkheidze <levanchx@gmail.com>
2023-05-24 21:57:37 +0400

    Inheritance & Polymorphism

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,25 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public Animal(){
+
+    }
+
+    public String getDescription(){
+        String part1 = "This animal is mostly " + color;
+        String part2 = (numberOfPaws==1)? ". It has " + numberOfPaws + " paw" : ". It has " + numberOfPaws + " paws";
+        String part3 = (hasFur)? " and a fur." : " and no fur.";
+
+        return part1 + part2 + part3;
+    }
 }
